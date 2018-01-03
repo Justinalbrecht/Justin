24644256f2f0e710fc2a2c54c6c30d110e6a47bb
Justinalbrecht <32962929+Justinalbrecht@users.noreply.github.com>
2018-01-03 23:56:46 +0000

    update
    This is how the basic movement in the game will follow.
    we first have a bit of dialogue to inform the player which directions they can take, then offer the player an input which asks "Where would you like to do?", this will then set the mars_choice variable to whatever the user has input; if the user has input either 'North' 'east' or 'west' it will print the "You go'' and the direction, if the user has input a invalid direction the computer will run the dialogue again and ask them again which direction they would like to go, until they input a valid direction.
    we then define  'North' 'east' and 'west' which more dialogue and more choices through defining and inputs.

--- game3.py
+++ game3.py
@@ -5,7 +5,7 @@ import time
 def airlockexit():
     mars_variable = ["north", "east", "west"]
     mars_choice = ""
-    while mars_choice not in mars_variable:
+    while mars_choice not in mars_variable:       #This will check if the user has input the correct string; if not, it will run again.
         print ("")
         print ("You exit the ship...")
         time.sleep(3)
@@ -19,18 +19,18 @@ def airlockexit():
         print ("")
         print ("West leading towards a large stack on storage containers.")
 
-        mars_choice = str(input("Where would you like to go? "))
+        mars_choice = str(input("Where would you like to go? "))      #here is where the player makes the choice of direciton.
     print ("")
-    print ("You go " + mars_choice)
+    print ("You go " + mars_choice)     #this code will print the valid direction if the user has put the correct string in.
 
-    if mars_choice == mars_variable[0]:
+    if mars_choice == mars_variable[0]:      #after the users inputs a valid string we then tell the code to either run the following.
         marsnorth()
     elif mars_choice == mars_variable[1]:
         marseast()
     elif mars_choice == mars_variable[2]:
         marswest()
 
-def marsnorth():
+def marsnorth():             # Here we define the direcitons.
     print ("")
     print ("1")
 
